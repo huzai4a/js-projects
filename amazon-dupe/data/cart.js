8c1ed9ac5cc2c769fc912c78a7b358ca64a810b1
Huzaifa Syed <huzaifa_syed78@hotmail.com>
2024-07-19 14:18:45 -0400

    created addToCart tests

--- amazon-dupe/data/cart.js
+++ amazon-dupe/data/cart.js
@@ -1,16 +1,21 @@
-export const cart = JSON.parse(localStorage.getItem('cartItems')) ||  
-[
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: '3'
-    }
-];
+export let cart;
 
+loadFromStorage();
+
+export function loadFromStorage(){
+     cart = JSON.parse(localStorage.getItem('cartItems')) ||  
+    [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: '3'
+        }
+    ];
+}
 
 function saveToStorage(){
     localStorage.setItem('cartItems', JSON.stringify(cart))
@@ -26,7 +31,8 @@ export function addToCart(productId) {
         }
     });
 
-    const quantity = Number(document.querySelector(`.js-select-${productId}`).value);
+    // const quantity = Number(document.querySelector(`.js-select-${productId}`).value);
+    const quantity = 1;
 
     if (dupeItem){
         dupeItem.quantity+= quantity;
---
+++ amazon-dupe/tests/tests-jasmine/data/cartTest.js
@@ -0,0 +1,55 @@
+import { addToCart, cart, loadFromStorage } from "../../../data/cart.js";
+
+describe('addToCart test suite', () => {
+    it('adds an existing product to cart', () => {
+        // to prevent effects on real code
+        spyOn(localStorage, 'setItem');
+
+        // gets empty string preventing real code effects
+        spyOn(localStorage, 'getItem').and.callFake(()=> {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1, 
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        // check if set item was called
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        (expect(cart[0].productId)).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        (expect(cart[0].quantity)).toEqual(2);
+    });
+
+    it('adds new product to cart', () => {
+        // to prevent effects on real code
+        spyOn(localStorage, 'setItem');
+        
+        // gets empty string preventing real code effects
+        spyOn(localStorage, 'getItem').and.callFake(()=> {
+            return JSON.stringify([]);
+        });
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        // check if set item was called
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        (expect(cart[0].productId)).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        (expect(cart[0].quantity)).toEqual(1);
+    });
+});
+
+    // it('new product added to cart', () => {
+    //     expect(formatCurrency(0)).toEqual('$0.00');
+    // });
+
+    // it('works with decimal round up', () => {
+    //     expect(formatCurrency(2000.5)).toEqual('$20.01');
+    // });
+
+    // it('works with decimal round down', () => {
+    //     expect(formatCurrency(2000.4)).toEqual('$20.00');
+    // });
