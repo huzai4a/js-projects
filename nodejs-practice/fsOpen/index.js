30e5b6e3671f3c50e42a75dfc8226d135cfd7532
Huzaifa Syed <huzaifa_syed78@hotmail.com>
2024-08-03 13:35:08 -0400

    installed express

--- nodejs-practice/fsOpen/index.js
+++ nodejs-practice/fsOpen/index.js
@@ -1,12 +1,28 @@
-// note: although node index works, usually npm projects execute that type of task as npm scripts
+const http = require('http')
 
-const http = require('http');
 
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Response ended');
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
 })
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = 8000;
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
